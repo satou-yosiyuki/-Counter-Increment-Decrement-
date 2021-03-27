188c7536930db1279d535091ce2a82163da03bc6
satou <stuysyk1027@icloud.com>
2021-03-28 03:31:37 +0900

    Login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
 import React from 'react';
-import Counter from './components/Counter/Counter';
 
-function App() {
-  return (
-    <div>
-     <h1>カウンター</h1>
-     <Counter />
-     </div>
-  );
+class App extends React.Component{
+  constructor(props){
+      super(props);
+      this.state={
+        isLoggedIn: false
+      };
+    }
+
+    login(){
+      this.setState({isLoggedIn: true});
+    }
+
+    logout(){
+      this.setState({isLoggedIn: false});
+    }
+    
+  render(){
+      if(this.state.isLoggedIn){
+        return(
+          <div>
+            <PrivatePage />
+            <button onClick={()=> this.logout()}>ログアウト</button>
+          </div>
+        );
+      }
+      return(
+        <div>
+          <PublicPage />
+          <button onClick={()=> this.login()}>ログイン</button>
+        </div>
+      );
+    }
 }
 
-export default App;
+  export default App;
+
+    const PublicPage=()=>{
+      const style={
+        backgroundColor: '#ff0'
+      };
+
+      return(
+        <div style={style}>
+          <h2>非ログイン時のページ</h2>
+        </div>
+      );
+    };
+
+    const PrivatePage=()=>{
+      const style={
+        backgroundColor: '#0f0'
+      };
+
+      return(
+        <div style={style}>
+          <h2>ログイン時のページ</h2>
+        </div>
+      );
+    };
